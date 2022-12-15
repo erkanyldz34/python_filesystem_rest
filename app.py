7b1844469a85993c2de0095528452ea7201d618d
erkanyldz34 <117810531+erkanyldz34@users.noreply.github.com>
2022-12-15 23:12:27 +0200

    Prototype of the system (routes signatures)

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 from flask import Flask
-from python_filesystem.filesystem import FileSystem
+from python_filesystem.filesystem import FileSystem, DELIMITER
+from python_filesystem.filesystem import BinaryFile, LogFile, BufferFile
+
 
 fs = FileSystem()
 app = Flask(__name__)
+app.config["FILESYSTEM_OBJ"] = fs
 
+
+@app.route("/", methods=["GET", "PUT", "DELETE"])
+def index():
+    pass
+
+
+@app.route("/directory", methods=["POST"])
+def directory():
+    pass
+
+
+@app.route("/binaryfile", methods=["GET", "POST"])
+def binaryfile():
+    pass
+
+
+@app.route("/logtextfile", methods=["GET", "POST", "PUT"])
+def logtextfile():
+    pass
+
+
+@app.route("/bufferfile", methods=["GET", "PUT", "POST"])
+def bufferfile():
+    pass
 
 
 if __name__ == "__main__":
